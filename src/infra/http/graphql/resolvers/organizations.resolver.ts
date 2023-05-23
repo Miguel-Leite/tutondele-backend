a81ed22539697b30c6fc93e3dca77ebc2885e5b4
Miguel Leite <miguelleite200leite@gmail.com>
2023-05-23 17:39:17 +0100

    ✨ feat(organization): Functionalities of the organization(school) module has been completed

--- src/infra/http/graphql/resolvers/organizations.resolver.ts
+++ src/infra/http/graphql/resolvers/organizations.resolver.ts
@@ -1,14 +1,25 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { OrganizationModel } from '../dtos/models/organization-model';
-import { CreateOrganizationInput } from '../dtos/inputs/create-organization-input';
+
 import { GetAllOrganizations } from '@app/use-cases/organizations/get-all-organizations';
 import { CreateOrganization } from '@app/use-cases/organizations/create-organization';
+import { UpdateOrganization } from '@app/use-cases/organizations/update-organization';
+import { RemoveOrganization } from '@app/use-cases/organizations/remove-organization';
+import { ActivateOrganization } from '@app/use-cases/organizations/activate-organization';
+import { DeactivateOrganization } from '@app/use-cases/organizations/deactivate-organization';
+
+import { OrganizationModel } from '../dtos/models/organization-model';
+import { UpdateOrganizationInput } from '../dtos/inputs/update-organization-input';
+import { CreateOrganizationInput } from '../dtos/inputs/create-organization-input';
 
 @Resolver(() => OrganizationModel)
 export class OrganizationsResolver {
   constructor(
     private getAllOrganizations: GetAllOrganizations,
     private createOrganization: CreateOrganization,
+    private updateOrganization: UpdateOrganization,
+    private removeOrganization: RemoveOrganization,
+    private activateOrganization: ActivateOrganization,
+    private deactivateOrganization: DeactivateOrganization,
   ) {}
   @Query(() => [OrganizationModel])
   async organizations() {
@@ -17,8 +28,32 @@ export class OrganizationsResolver {
   }
 
   @Mutation(() => OrganizationModel)
-  async organizationCreate(@Args('data') data: CreateOrganizationInput) {
+  async addOrganization(@Args('data') data: CreateOrganizationInput) {
     const { organization } = await this.createOrganization.execute(data);
+    return organization;
+  }
+
+  @Mutation(() => OrganizationModel)
+  async editOrganization(@Args('data') data: UpdateOrganizationInput) {
+    const { organization } = await this.updateOrganization.execute(data);
+    return organization;
+  }
+
+  @Mutation(() => OrganizationModel)
+  async enableOrganization(@Args('id') id: string) {
+    const { organization } = await this.activateOrganization.execute(id);
+    return organization;
+  }
+
+  @Mutation(() => OrganizationModel)
+  async disableOrganization(@Args('id') id: string) {
+    const { organization } = await this.deactivateOrganization.execute(id);
+    return organization;
+  }
+
+  @Mutation(() => OrganizationModel)
+  async deleteOrganization(@Args('id') id: string) {
+    const { organization } = await this.removeOrganization.execute(id);
     return organization;
   }
 }
